20f0c467f2d81e583f1f7ef78612d79ef41b424b
harinaathan <62490153+harinaathan@users.noreply.github.com>
2022-08-13 15:36:26 +0530

    Update myScores.py

--- myScores.py
+++ myScores.py
@@ -2,30 +2,33 @@
 # coding: utf-8
 
 # In[1]:
-
-
 import pandas as pd
 import plotly.graph_objects as go
+import streamlit as st
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set_theme()
-# In[3]:
-
-
+# In[2]:
 df = pd.read_excel("scoreLog.xlsx")
 dfP = df.copy()
-dfP["My Score"] = dfP["My Score"].div(dfP["Total Score"])
-dfP["Batch Median Score"] = dfP["Batch Median Score"].div(dfP["Total Score"])
-dfP["Batch Max Score"] = dfP["Batch Max Score"].div(dfP["Total Score"])
+dfP["My Score"] = dfP["My Score"].div(dfP["Total Score"]).round(3)
+dfP["Batch Median Score"] = dfP["Batch Median Score"].div(dfP["Total Score"]).round(3)
+dfP["Batch Max Score"] = dfP["Batch Max Score"].div(dfP["Total Score"]).round(3)
 dfP = dfP[["Course","My Score","Batch Median Score","Batch Max Score"]]
 
+# In[3]:
+fig = go.Figure()
+fig.add_trace(go.Bar(x=dfP.Course,y=dfP["My Score"],name="My Score",hovertemplate="My Score<br>%{y}"))
+fig.add_trace(go.Scatter(x=dfP.Course,y=dfP["Batch Median Score"],fillcolor='rgba(99, 110, 250,1)',
+                         name="Batch Median Score",hovertemplate="Batch Median Score<br>%{y}"))
+fig.add_trace(go.Scatter(x=dfP.Course,y=dfP["Batch Max Score"],fillcolor='rgba(239, 85, 59,1)',
+                         name="Batch Max Score",hovertemplate="Batch Max Score<br>%{y}"))
+fig.update_yaxes(title=dict(text="Score",standoff=0),
+                 type='log',tickformat='0%')
+fig.update_xaxes(title=dict(text="Modules",standoff=0))
+fig.update_layout(legend=dict(yanchor='bottom',y=1,orientation='h',xanchor='right',x=1))
+fig.update_layout(title=dict(text='My learning curve through the course',
+                             yanchor='top',y=0.9,xanchor='left',x=0))
+fig.update_layout(autosize=False,height=500,width=500);
 
 # In[4]:
-
-
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=dfP.Course,y=dfP["Batch Median Score"],mode='none',fill='tozeroy',name="Batch Median Score"))
-fig.add_trace(go.Scatter(x=dfP.Course,y=dfP["Batch Max Score"],mode='none',fill='tonexty',name="Batch Max Score"))
-fig.add_trace(go.Bar(x=dfP.Course,y=dfP["My Score"],))
-
+st.title('Check this out')
+st.plotly_chart(fig)
